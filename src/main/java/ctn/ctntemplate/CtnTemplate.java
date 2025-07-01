e57697eed499b941e259510124a58bc217537f30
Wang-Xiao-Jing <1809315974@qq.com>
2025-07-01 10:25:59 +0800

    优化

--- src/main/java/ctn/ctntemplate/CtnTemplate.java
+++ src/main/java/ctn/ctntemplate/CtnTemplate.java
@@ -7,17 +7,8 @@ import com.mojang.logging.LogUtils;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -30,10 +21,17 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
+
+import static ctn.ctntemplate.init.ArmorMaterials.ARMOR_MATERIALS_TYPE_REGISTER;
+import static ctn.ctntemplate.init.Blocks.BLOCK_REGISTER;
+import static ctn.ctntemplate.init.CreativeModeTab.PROJECT_MOON_TAB_REGISTER;
+import static ctn.ctntemplate.init.EntityAttributes.ATTRIBUTE_REGISTER;
+import static ctn.ctntemplate.init.Entitys.ENTITY_TYPE_REGISTER;
+import static ctn.ctntemplate.init.ItemDataComponents.ITEM_DATA_COMPONENT_REGISTER;
+import static ctn.ctntemplate.init.Items.ITEM_REGISTER;
+import static ctn.ctntemplate.init.MenuTypes.MENU_TYPE_REGISTER;
+import static ctn.ctntemplate.init.ParticleTypes.PARTICLE_TYPE_REGISTER;
+import static ctn.ctntemplate.init.SoundEvents.SOUND_EVENT_TYPE_REGISTER;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(CtnTemplate.MODID)
@@ -42,51 +40,28 @@ public class CtnTemplate {
     public static final String MODID = "ctntemplate";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "ctntemplate" namespace
-    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "ctntemplate" namespace
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "ctntemplate" namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
-
-    // Creates a new Block with the id "ctntemplate:example_block", combining the namespace and path
-    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
-    // Creates a new BlockItem with the id "ctntemplate:example_block", combining the namespace and path
-    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
-
-    // Creates a new food item with the id "ctntemplate:example_id", nutrition 1 and saturation 2
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
-            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
-
-    // Creates a creative tab with the id "ctntemplate:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.ctntemplate")) //The language key for the title of your CreativeModeTab
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-            }).build());
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public CtnTemplate(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
-
+        
+        BLOCK_REGISTER.register(modEventBus);
+        ITEM_REGISTER.register(modEventBus);
+        PROJECT_MOON_TAB_REGISTER.register(modEventBus);
+        ATTRIBUTE_REGISTER.register(modEventBus);
+        PARTICLE_TYPE_REGISTER.register(modEventBus);
+        ARMOR_MATERIALS_TYPE_REGISTER.register(modEventBus);
+        SOUND_EVENT_TYPE_REGISTER.register(modEventBus);
+        ENTITY_TYPE_REGISTER.register(modEventBus);
+        ITEM_DATA_COMPONENT_REGISTER.register(modEventBus);
+        MENU_TYPE_REGISTER.register(modEventBus);
+        
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (CTNTemplate) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, CtnConfig.SPEC);
@@ -105,13 +80,6 @@ public class CtnTemplate {
         CtnConfig.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(EXAMPLE_BLOCK_ITEM);
-        }
-    }
-
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
@@ -120,7 +88,7 @@ public class CtnTemplate {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
--- src/main/java/ctn/ctntemplate/init/EntityAttributes.java
+++ src/main/java/ctn/ctntemplate/init/EntityAttributes.java
@@ -10,16 +10,16 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import static ctn.ctntemplate.CtnTemplate.MODID;
 
 public class EntityAttributes {
-	public static final DeferredRegister<Attribute> PM_ATTRIBUTE_REGISTER             = DeferredRegister.create(Registries.ATTRIBUTE, MODID);
+	public static final DeferredRegister<Attribute> ATTRIBUTE_REGISTER = DeferredRegister.create(Registries.ATTRIBUTE, MODID);
 	/** 物理伤害抵抗能力 */
 //	public static final Holder<Attribute>           PHYSICS_RESISTANCE                = registerRangedAttribute("generic.physics_resistance", "attribute.name.generic.physics_resistance", 1.0, -1024, 1024);
 
 	
 	public static Holder<Attribute> registerRangedAttribute(String name, String descriptionId, double defaultValue, double min, double max) {
-		return PM_ATTRIBUTE_REGISTER.register(name, () -> new RangedAttribute(MODID + "." + descriptionId, defaultValue, min, max).setSyncable(true));
+		return ATTRIBUTE_REGISTER.register(name, () -> new RangedAttribute(MODID + "." + descriptionId, defaultValue, min, max).setSyncable(true));
 	}
 	
 	public static Holder<Attribute> registerBooleanAttribute(String name, String descriptionId, boolean defaultValue) {
-		return PM_ATTRIBUTE_REGISTER.register(name, () -> new BooleanAttribute(MODID + "." + descriptionId, defaultValue).setSyncable(true));
+		return ATTRIBUTE_REGISTER.register(name, () -> new BooleanAttribute(MODID + "." + descriptionId, defaultValue).setSyncable(true));
 	}
 }
